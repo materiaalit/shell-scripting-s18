b25bd9dc4e67a96af5456b40c0629f854ab8bf23
Henrik Nygren <nygrenh@gmail.com>
2018-08-30 22:45:25 +0300

    Update travis.yml

--- assets/javascripts/login-modal.js
+++ assets/javascripts/login-modal.js
@@ -21,7 +21,7 @@ class LoginModal {
 
     if(client.getUser()) {
       this.afterLogin();
-    } else if(window.location.pathname !== '/' && window.location.pathname !== '/ohjelmointi-18/') {
+    } else if(window.location.pathname !== '/' && window.location.pathname !== '/ohjelmointi-s18/') {
       this.loginModalNode.modal('show');
     }
 
